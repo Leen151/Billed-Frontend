3bd36953722f26220076caa4e217b7b8ed22a236
Leen151 <leen.m@orange.fr>
2024-12-21 20:03:09 +0100

    Correction test tri des dates : modification du test afin qu'il test ce qui se passe réelement sur l'application
    - instanciation de la classe Bills et appel de la méthode d'instance getBills() afin d'avoir le même processus de formatage des données que dans l'application
    - modification de la regex afin de trouver le format de date affiché à l'écran de l'application et dans le DOM créé pour le test
    - le dates sont parsées en objet Dates afin de les trier (on ne peut par trier des dates en format string)

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -4,9 +4,11 @@
 
 import {screen, waitFor} from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
+import Bills from "../containers/Bills.js";
 import { bills } from "../fixtures/bills.js"
 import { ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
+import mockedBills from "../__mocks__/store.js"
 
 import router from "../app/Router.js";
 
@@ -28,11 +30,29 @@ describe("Given I am connected as an employee", () => {
       //to-do write expect expression
 
     })
-    test("Then bills should be ordered from earliest to latest", () => {
-      document.body.innerHTML = BillsUI({ data: bills })
-      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
-      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
-      const datesSorted = [...dates].sort(antiChrono)
+    test("Then bills should be ordered from earliest to latest", async () => {
+      // Créer une instance de Bills
+      const billsInstance = new Bills({
+        document,
+        onNavigate: () => {},
+        store: { 
+          bills: () => ({ 
+            list: () => Promise.resolve(bills)  // on utilise nos données de tests
+          })
+        },
+        localStorage: window.localStorage
+      });
+      //on appele la methode getBills pour formatter les données comme dans l'application
+      const billsObject = await billsInstance.getBills();
+      
+      document.body.innerHTML = BillsUI({ data: billsObject })
+      const dates = screen.getAllByText(/^(0[1-9]|[12][0-9]|3[01])\s([A-Za-z]{3})\.\s(19|20)\d\d$/i).map(a => a.innerHTML)
+
+      //on ne peut pas comparer des dates sous forme de chaine de caractère
+      //il faut les transformer en objet Date
+      //et les dates sont affichés sous forme "29 Nov. 2024", il faut enlever le "." pour les transformer en date
+      const datesSorted = [...dates].sort((a, b) => new Date(b.replace('.', '')) - new Date(a.replace('.', '')));
+
       expect(dates).toEqual(datesSorted)
     })
   })
