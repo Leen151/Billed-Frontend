dc59dc14946f0e38d2a453573d3a61a6ce8edec2
Leen151 <leen.m@orange.fr>
2024-12-21 19:43:42 +0100

    Correction test tri des dates : correction du formatage des dates pour avoir l'année sur 4 chiffres
    (cela permet de parser en objet Date)

--- src/app/format.js
+++ src/app/format.js
@@ -3,8 +3,9 @@ export const formatDate = (dateStr) => {
   const ye = new Intl.DateTimeFormat('fr', { year: 'numeric' }).format(date)
   const mo = new Intl.DateTimeFormat('fr', { month: 'short' }).format(date)
   const da = new Intl.DateTimeFormat('fr', { day: '2-digit' }).format(date)
+  
   const month = mo.charAt(0).toUpperCase() + mo.slice(1)
-  return `${parseInt(da)} ${month.substr(0,3)}. ${ye.toString().substr(2,4)}`
+  return `${da} ${month.slice(0,3)}. ${ye}`
 }
  
 export const formatStatus = (status) => {
